e2bf94cd4f8e84f52763054e487408777808c6e4
seohee <test@test.123>
2023-06-22 22:03:00 +0900

    this_private

---
+++ src/this_/MainClass02.java
@@ -0,0 +1,21 @@
+package this_;
+
+class TestClass02{
+	private int num;	//은닉화
+	public void test(int n) {
+		num = n;
+	}
+	public int test2() {
+		return num;
+	}
+}
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		TestClass02 t = new TestClass02();
+		int n = 12345;
+		t.test(n);
+		int re = t.test2();
+		System.out.println("결과 : "+re);
+	}
+}
